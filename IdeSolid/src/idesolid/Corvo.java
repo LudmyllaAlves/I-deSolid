212dc1ebcf390cb5a582acd9480d3535ddfa386e
LudmyllaAlves <85032725+LudmyllaAlves@users.noreply.github.com>
2021-06-01 19:38:13 -0300

    Add files via upload

---
+++ IdeSolid/src/idesolid/Corvo.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idesolid;
+
+/**
+ *
+ * @author tatia
+ */
+public class Corvo implements AvesqueVoam{
+    String nome;
+    int numeroDeIden;
+
+    @Override
+    public int localização() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int altitude() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int renderizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ IdeSolid/src/idesolid/Pinguim.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package idesolid;
+
+/**
+ *
+ * @author tatia
+ */
+public class Pinguim implements Aves{
+    String nome;
+
+    @Override
+    public int localização() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+
+    @Override
+    public int renderizar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
